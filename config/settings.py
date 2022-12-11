2645feb9a505c0db9bfd771ac1af24f8f0f275c2
Abdulla Abdukulov <abduqulovabdulla3108@gmail.com>
2022-12-11 13:21:35 +0500

    changed by Abdullah

--- config/settings.py
+++ config/settings.py
@@ -129,7 +129,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MODELS = os.path.join(BASE_DIR, 'ml/models')
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:9090'
+    'http://localhost:8080'
 
 ]
 CORS_ALLOW_HEADERS = default_headers + (
